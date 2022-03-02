54fe1d3173189294ef946acfa1e3fe9a14821acb
Xuuu <1753059613@qq.com>
2022-03-02 23:58:10 +0800

    NEW! 23:58

--- Code/main.c
+++ Code/main.c
@@ -1,13 +1,18 @@
 #include <REG52.H>
-
+#include <STDIO.H>
+sbit P3_4 = P3 ^ 4;
+sbit P2_0 = P2 ^ 0;
 void main()
 {
 	while (1)
 	{
-		
-
-
-		
+		if(P2_0 == 0)
+		{
+			P3_4 = 0;
+			
+		}else if(P2_0 == 1){
+			P3_4 = 1;
+		}
 	}
 	
 }
